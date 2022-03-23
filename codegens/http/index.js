ed652e05a418ae161d5f9c0be5377130908de8c9
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2022-03-23 11:26:37 -0400

    Disable unused language

--- codegens/http/index.js
+++ codegens/http/index.js
@@ -1,6 +1,8 @@
-let converter = require('./lib/code-http-converter');
+// Uncomment lines #2-#7 and delete line #8 to enable language
+// let converter = require('./lib/code-http-converter');
 
-module.exports = {
-  getOptions: converter.getOptions,
-  convert: converter.convert
-};
+// module.exports = {
+//   getOptions: converter.getOptions,
+//   convert: converter.convert
+// };
+module.exports = require('');
--- codegens/js-fetch/index.js
+++ codegens/js-fetch/index.js
@@ -1,1 +1,3 @@
-module.exports = require('./lib');
+// Uncomment line #2 and delete line #3 to enable language
+// module.exports = require('./lib');
+module.exports = require('');
